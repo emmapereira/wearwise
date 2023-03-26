c2307fbf9890e9538b3cfe41b6367d3c9db5f1b7
emmapereira <emmapereira23@gmail.com>
2023-03-26 14:44:28 +0200

    the clothing items are showing!!!!

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -5,28 +5,29 @@ class ClothingItem {
   final String id;
   final String name;
   final String type;
-  final String color;
-  final String season;
+  final String colour;
+  final List<String> seasons;
 
   ClothingItem({
     required this.id,
     required this.name,
     required this.type,
-    required this.color,
-    required this.season,
+    required this.colour,
+    required this.seasons,
   });
 }
 
 Future<List<ClothingItem>> getClothingItems() async {
-  final String data = await rootBundle.loadString('assets/clothing_items.json');
-  final List<dynamic> jsonList = json.decode(data);
+  final String data =
+      await rootBundle.loadString('lib/assets/clothing_items.json');
+  final List<dynamic> jsonList = json.decode(data)['data'];
   return jsonList
       .map((item) => ClothingItem(
             id: item['id'],
             name: item['name'],
             type: item['type'],
-            color: item['color'],
-            season: item['season'],
+            colour: item['colour'],
+            seasons: (item['seasons'] as List<dynamic>).cast<String>(),
           ))
       .toList();
 }
--- lib/screens/closet.dart
+++ lib/screens/closet.dart
@@ -19,6 +19,7 @@ class _ClosetState extends State<Closet> {
   void initState() {
     super.initState();
     _futureClothingItems = getClothingItems();
+    print(_futureClothingItems);
   }
 
   @override
@@ -37,35 +38,38 @@ class _ClosetState extends State<Closet> {
           ),
         ),
         Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(25.0),
-                  ),
-                  child: const TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Search',
-                      contentPadding: EdgeInsets.symmetric(
-                        horizontal: 20.0,
-                        vertical: 15.0,
+          padding: const EdgeInsets.only(bottom: 10.0),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search',
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 20.0,
+                          vertical: 15.0,
+                        ),
+                        border: InputBorder.none,
+                        suffixIcon: Icon(Icons.search),
                       ),
-                      border: InputBorder.none,
-                      suffixIcon: Icon(Icons.search),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(width: 10.0),
-              IconButton(
-                icon: Icon(Icons.tune),
-                color: Color(0xff865537),
-                onPressed: () {},
-              ),
-            ],
+                SizedBox(width: 10.0),
+                IconButton(
+                  icon: Icon(Icons.tune),
+                  color: Color(0xff865537),
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
         ),
         Expanded(
@@ -82,11 +86,70 @@ class _ClosetState extends State<Closet> {
                     itemCount: snapshot.data!.length,
                     itemBuilder: (BuildContext context, int index) {
                       final clothingItem = snapshot.data![index];
+
+                      Color tagColor = Colors.grey;
+                      if (clothingItem.type == "jacket")
+                        tagColor = Colors.brown;
+                      else if (clothingItem.type == "jeans")
+                        tagColor = Colors.green;
+                      else if (clothingItem.type == "t-shirt")
+                        tagColor = Colors.deepOrange;
+                      else if (clothingItem.type == "skirt")
+                        tagColor = Colors.purple;
+                      else if (clothingItem.type == "shoes")
+                        tagColor = Colors.red;
+                      else if (clothingItem.type == "coat")
+                        tagColor = Colors.blue;
+
                       return GridTile(
-                        child: Container(
-                          color: Colors.grey[200],
-                          child: Center(
-                            child: Text(clothingItem.name),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 2,
+                                  blurRadius: 5,
+                                  offset: const Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    clothingItem.name,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12.0,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 8.0),
+                                  child: Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8.0, vertical: 4.0),
+                                    decoration: BoxDecoration(
+                                      color: tagColor,
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    child: Text(
+                                      clothingItem.type,
+                                      style: const TextStyle(
+                                        fontSize: 10.0,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       );
